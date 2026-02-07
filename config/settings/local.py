6e8ab72855c6f82980113fdd31eac7519f0e91d6
dera1992 <30830532+dera1992@users.noreply.github.com>
2026-02-07 10:38:51 +0000

    Improve login flow messaging

--- config/settings/local.py
+++ config/settings/local.py
@@ -80,3 +80,11 @@ CELERY_TASK_EAGER_PROPAGATES = True
 # ------------------------------------------------------------------------------
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:5173",
+]
+
+REQUIRE_EMAIL_VERIFICATION = False
